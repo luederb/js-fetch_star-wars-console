b3c3b00ee24b852ce464f8ace216665169bf6785
Lueder Budde <lueder.budde@gmail.com>
2023-10-10 16:42:51 +0200

    finished challenge

--- js/index.js
+++ js/index.js
@@ -2,6 +2,22 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.count);
+  console.log(`data: ${data} \ncount: ${data.count} \nnext: ${data.next}`);
+  console.log(
+    "data: ",
+    data,
+    "\ncount: ",
+    data.count,
+    "\nnext: ",
+    data.next,
+    "\ncolor of R2-D2's eye: ",
+    data.results[2].eye_color
+  );
+}
 
 fetchData();
